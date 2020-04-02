945c4581f7cb78a2f6fd8807a6a226f3f3a1366e
Cristian Henrici <henricri@students.zhaw.ch>
2020-04-02 17:35:54 +0200

    fixed URL for CORS Request

--- CDCServer/src/main/java/com/cristan/CDCServer/CDCServer/controller/ComputerController.java
+++ CDCServer/src/main/java/com/cristan/CDCServer/CDCServer/controller/ComputerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ComputerController {
 
     ComputerRepository repository;
@@ -30,3 +30,4 @@ public class ComputerController {
         repository.save(computer);
     }
 }
+
