253466e44aa51edf882dbbad688a43a11459c0bc
Cristian Henrici <henricri@students.zhaw.ch>
2020-04-14 18:12:40 +0200

    changed urls

--- CDCServer/src/main/java/com/cristan/CDCServer/CDCServer/controller/ComputerController.java
+++ CDCServer/src/main/java/com/cristan/CDCServer/CDCServer/controller/ComputerController.java
@@ -6,6 +6,7 @@ import com.cristan.CDCServer.CDCServer.model.Computer;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -17,15 +18,40 @@ public class ComputerController {
         this.repository = repository;
     }
 
-    @GetMapping("/computers")
+    @GetMapping("/computers/fetchAll")
+    @ResponseBody
     public List<Computer> getComputers() {
-        return (List<Computer>) repository.findAll();
+        List<Computer> result = (List<Computer>) repository.findAll();
+        System.out.println("Fetch all result: " +  result.toString());
+        return result;
     }
 
-    @PostMapping("/computers")
-    public void saveComputer(@RequestBody Computer computer) {
-        repository.save(computer);
-        System.out.println("Computer: " +  computer);
+    @PostMapping("/computers/save")
+    @ResponseBody
+    public Computer saveComputer(@RequestBody Computer computer) {
+        System.out.println("Computer before insert: " +  computer.toString());
+        Computer result =repository.save(computer);
+        System.out.println("Computer after: " +  result.toString());
+        return result;
+    }
+
+    @PutMapping("/computers/update")
+    @ResponseBody
+    public Optional<Computer> updateComputer(@RequestBody Computer newComputer) {
+        System.out.println("Computer updateComputer: " +  newComputer.toString());
+        long id = newComputer.getId();
+        return repository.findById(id).map(
+                computer -> {
+                    computer.setComputerName(newComputer.getComputerName());
+                    computer.setUserName(newComputer.getUserName());
+                    computer.setUserSurname(newComputer.getUserSurname());
+                    computer.setDescription(newComputer.getDescription());
+                    computer.setWinVersion(newComputer.getWinVersion());
+                    computer.setDomainMigration(newComputer.getDomainMigration());
+                    computer.setLastUpdate(newComputer.getLastUpdate());
+                    return repository.save(computer);
+                }
+        );
     }
 }
 
