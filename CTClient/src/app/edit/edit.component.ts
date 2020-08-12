ec8a8aa63e62b201c412c845c55eb74a4cfeac80
Cristian Henrici <henricri@students.zhaw.ch>
2020-08-12 11:24:00 +0200

    fixed edit form to show all input values

--- CTClient/src/app/edit/edit.component.ts
+++ CTClient/src/app/edit/edit.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { ComputerService } from 'src/app/core/computer.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Computer } from 'src/app/model/computer';
@@ -11,17 +10,17 @@ import { Computer } from 'src/app/model/computer';
 })
 export class EditComponent implements OnInit {
   computerForm = new FormGroup({
-    computerName: new FormControl(''),
-    description: new FormControl(''),
-    userName: new FormControl(''),
-    userSurname: new FormControl(''),
-    winVersion: new FormControl(''),
-    lastUpdate: new FormControl(''),
-    domainMigration: new FormControl('')
+    computerName: new FormControl(),
+    description: new FormControl(),
+    userName: new FormControl(),
+    userSurname: new FormControl(),
+    winVersion: new FormControl(),
+    lastUpdate: new FormControl(),
+    domainMigration: new FormControl()
   });
   computer: Computer;
 
-  constructor(private service: ComputerService, public dialogRef: MatDialogRef<EditComponent>,
+  constructor(public dialogRef: MatDialogRef<EditComponent>,
               private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) private data: any) {
                 if (data.act === 'EDIT') {
                   console.log('data.dataKey: ' + data.dataKey.computerName);
@@ -33,15 +32,16 @@ export class EditComponent implements OnInit {
     if (this.data.act === 'EDIT') {
         console.log('Computer DataKey Id: ' + this.data.dataKey.id);
         console.log('Computer id: ' + this.computer.id);
+        console.log('Computer lastUpdate: ' + this.data.dataKey.lastUpdate);
         this.computerForm = this.fb.group({
-        id: [this.data.dataKey.id, []],
-        computerName: [this.data.dataKey.computerName, []],
-        description: [this.data.dataKey.description, []],
-        userName: [this.data.dataKey.userName, []],
-        userSurname: [this.data.dataKey.userSurname, []],
-        winVersion: [this.data.dataKey.winVersion, []],
-        lastUpdate: [this.data.dataKey.lastUpdate, []],
-        domainMigration: [this.data.dataKey.domainMigration, []]
+        id: [this.data.dataKey.id],
+        computerName: [this.data.dataKey.computerName],
+        description: [this.data.dataKey.description],
+        userName: [this.data.dataKey.userName],
+        userSurname: [this.data.dataKey.userSurname],
+        winVersion: [this.data.dataKey.winVersion],
+        lastUpdate: [new Date(this.data.dataKey.lastUpdate)], // new Date() to get the input to the datepicker
+        domainMigration: [this.data.dataKey.domainMigration]
       });
     }
   }
